e32fb859833b3ed2f11fe1c04aac6822a6ffd26f
antoronson <antoronson590@gmail.com>
2024-07-10 07:38:05 +0200

    Service Tutorial executed

--- ROS2_Tutorials/ros2_ws/build/py_srvcli/prefix_override/sitecustomize.py
+++ ROS2_Tutorials/ros2_ws/build/py_srvcli/prefix_override/sitecustomize.py
@@ -1,3 +1,4 @@
 import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/antoronson/GitTrade/GazeboTutorials/ROS2_Tutorials/ros2_ws/install/py_srvcli'
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/antoronson/GitTrade/GazeboTutorials/ROS2_Tutorials/ros2_ws/install/py_srvcli'
